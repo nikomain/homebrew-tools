f8f9b41c787d6d58f69e4b691b13bb8843d66e97
nikomain <nikolai.main@youlend.com>
2025-05-01 12:09:47 +0000

    Update telehelper formula to 1.0.0

--- Formula/telehelper.rb
+++ Formula/telehelper.rb
@@ -1,8 +1,8 @@
 class Telehelper < Formula
   desc ""
   homepage "https://github.com/nikomain/brew-scripts"
-  url "https://github.com/nikomain/brew-scripts/archive/refs/tags/telehelper-v1.0.8.tar.gz"
-  sha256 "5faecaa8c49e0693756793531a0b363e628a1828981c94d60a3b216cde1e2236"
+  url "https://github.com/nikomain/brew-scripts/archive/refs/tags/telehelper-v1.0.0.tar.gz"
+  sha256 "982d8245ed33b0452c848f0ceeb84bd9a7e4eae004bd31ac120a4b4ff5eb1d22"
   license ""
 
   def install
